e1fa91081e41d1025f4206733d54532702572e1a
Lou <LouXu@poweronlabs.ai>
2025-12-23 15:40:47 +0800

    refactor: fix shadcn/ui command and import path & add some ui components

---
+++ src/renderer/src/components/ui/skeleton.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@renderer/shared/utils'
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('bg-muted animate-pulse rounded-md', className)} {...props} />
+}
+
+export { Skeleton }
